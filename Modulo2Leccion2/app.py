a41c210b100832a4ce4c9c6c14d6ea349714cbb6
Trashboat4444 <kgvazquez05@gmail.com>
2025-04-19 23:00:43 -0500

    Update

--- Modulo2Leccion2/app.py
+++ Modulo2Leccion2/app.py
@@ -1,12 +1,22 @@
-from flask import Flask, request
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "mi_clave_secreta"
 
-@app.route("/login", methods=["POST"])
+class LoginForm(FlaskForm):
+    username = StringField("Nombre de Usuario", validators=[DataRequired(), Length(min=3)])
+    password = PasswordField("Contraseña", validators=[DataRequired()])
+    submit = SubmitField("Iniciar Sesión")
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    username = request.form["username"]
-    password = request.form["password"]
-    return f"Usuario: {username}, Contraseña: {password}"
+    form = LoginForm()
+    if form.validate_on_submit():
+        return f"Usuario: {form.username.data}"
+    return render_template("login.html", form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
